241b6125ef3848d4504225858e0357c83cbe39d8
pswoodworth <pswoodworth@gmail.com>
2017-01-10 00:47:15 -0500

    fixed testing syntax

--- test.js
+++ test.js
@@ -2,26 +2,40 @@ import { expect } from 'chai';
 import { toSeconds, toReadableTime } from '.';
 
 
-describe('toSeconds', () => {
-  expect( toSeconds( 2, '05', 'pm' ) ).to.equal(50700);
-  expect( toSeconds( 12, '00', 'am' ) ).to.equal(0);
-  expect( toSeconds( 12, 15, 'aM' ) ).to.equal(900);
-  expect( toSeconds( '03', '15', 'AM' ) ).to.equal(11700);
-  expect( toSeconds( -1, '00', 'AM' ) ).to.equal( toSeconds( 11, '00', 'PM' ) );
-  expect( toSeconds( '13', '05', 'AM' ) ).to.equal( toSeconds( 1, '05', 'PM' ) );
-  expect( toSeconds( 2, -5, 'AM' ) ).to.equal( toSeconds( 1, 55, 'AM' ) );
-  expect( toSeconds( 2, -68, 'AM' ) ).to.equal( toSeconds( 12, '52', 'AM' ) );
+describe('To seconds from values', () => {
+  const tests = [
+    { args: [ 2, '05', 'pm' ],   expected: 50700 },   
+    { args: [ 12, '00', 'am' ],  expected: 0 },     
+    { args: [ 12, 15, 'aM' ],    expected: 900 },   
+    { args: [ '03', '15', 'AM'], expected: 11700 },     
+    { args: [ '12', '15', 'PM'], expected: 44100 },     
+    { args: [ -1, '00', 'AM'],   expected: toSeconds( 11, '00', 'PM' ) }, 
+    { args: [ '13', '05', 'AM'], expected: toSeconds( 1, '05', 'PM' ) },
+    { args: [ 2, -5, 'AM' ],     expected: toSeconds( 1, 55, 'AM' ) }, 
+    { args: [ 2, -68, 'AM' ],    expected: toSeconds( 12, '52', 'AM' ) }
+  ];
+  tests.forEach(function(test) {
+    it(`should convert ${test.args} to seconds`, () => {
+      expect( toSeconds(...test.args)).to.equal( test.expected );
+    });
+  })
 });
 
-describe('toReadableTime', ()=> {
-  expect( toReadableTime( 50700 ) ).to.deep.equal({ hours: '2', minutes: '05', meridian: 'PM' });
-  expect( toReadableTime( 0 ) ).to.deep.equal({ hours: '12', minutes: '00', meridian: 'AM' });
-  expect( toReadableTime( 900 ) ).to.deep.equal({ hours: '12', minutes: '15', meridian: 'AM' });
-  expect( toReadableTime( 11700 ) ).to.deep.equal({ hours: '3', minutes: '15', meridian: 'AM' });
-  expect( toReadableTime( 91800 ) ).to.deep.equal({ hours: '1', minutes: '30', meridian: 'AM' });
-  expect( toReadableTime( -3600 ) ).to.deep.equal({ hours: '11', minutes: '00', meridian: 'PM' });
+
+describe('To values from seconds', () => {
+  const tests = [
+    { value: 50700, expected: { hours: '2', minutes: '05', meridian: 'PM' } },   
+    { value: 0,     expected: { hours: '12', minutes: '00', meridian: 'AM' } },     
+    { value: 900,   expected: { hours: '12', minutes: '15', meridian: 'AM' } },   
+    { value: 11700, expected: { hours: '3', minutes: '15', meridian: 'AM' } },     
+    { value: 91800, expected: { hours: '1', minutes: '30', meridian: 'AM' } },     
+    { value: -3600, expected: { hours: '11', minutes: '00', meridian: 'PM' } }
+  ];
+  tests.forEach(function(test) {
+    it(`should convert ${test.value} to value`, () => {
+      expect( toReadableTime(test.value)).to.deep.equal( test.expected );
+    });
+  });
 });
 
-describe('roundTrip', () => {
-  expect( toReadableTime( toSeconds( 2, '05', 'pm' ) ) ).to.deep.equal({ hours: '2', minutes: '05', meridian: 'PM' });
-});
+
